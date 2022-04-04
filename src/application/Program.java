089fddc2649b120cb5988d60798d3670a35200e7
elaniacs <carensousaesilva@gmail.com>
2022-04-04 17:54:20 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,15 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println();
+		System.out.println("=== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
 	}
+	
+	
 
 }
